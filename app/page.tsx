3e1564a22b31595cd1cdb0e04a6ef46f197a5754
Semih Orhan <sorhan3.14@gmail.com>
2023-10-16 21:22:13 +0300

    add auth to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <Button variant={'secondary'}>I am button</Button>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
