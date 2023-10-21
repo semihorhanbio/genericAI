e4ddf70551a4648cf5bee8638944efbae1c80053
Semih Orhan <sorhan3.14@gmail.com>
2023-10-21 14:18:02 +0300

    finish music AI

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,25 +1,35 @@
-import Image from "next/image";
-import { Separator } from "@/components/ui/separator";
+import { Icon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 
 interface HeadingProps {
-    title: string;
-    description: string;
-    src: string;
+  title: string;
+  description: string;
+  icon: Icon;
+  iconColor?: string;
+  bgColor?: string;
 }
 
-export const Heading = ({title, description, src}: HeadingProps) => {
+export const Heading = ({
+  title,
+  description,
+  icon: Icon,
+  iconColor,
+  bgColor,
+}: HeadingProps) => {
   return (
     <>
-        <div className="px-4 lg:px-8 flex items-center gap-x-2">
-            <div className="relative h-20 w-20">
-                <Image src={src} fill alt="Icon"/>
-            </div>
-            <div>
-                <h2 className="text-3xl font-bold">{title}</h2>
-                <p className="text-sm text-muted-foreground">{description}</p>
-            </div>
+      <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+          <Icon className={cn("w-10 h-10", iconColor)} />
         </div>
-        <Separator className="my-8"/>
+        <div>
+          <h2 className="text-3xl font-bold">{title}</h2>
+          <p className="text-sm text-muted-foreground">
+            {description}
+          </p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
