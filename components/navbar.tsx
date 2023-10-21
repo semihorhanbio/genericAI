2de2274d1cd88669c6bc1e1da1d0724646a2c441
Semih Orhan <sorhan3.14@gmail.com>
2023-10-21 19:34:14 +0300

    finish pro modal ui

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,21 @@
-import UserNavigation from "@/components/user-navigation"
-import { MobileSidebar } from "@/components/mobile-sidebar"
+import { UserButton } from "@clerk/nextjs";
 
-import React from 'react'
+import { MobileSidebar } from "@/components/mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
-const Navbar = () => {
-  return (
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
+
+  return ( 
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       <div className="flex w-full justify-end">
-        <UserNavigation/>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
-  )
+   );
 }
-
-export default Navbar
+ 
+export default Navbar;
